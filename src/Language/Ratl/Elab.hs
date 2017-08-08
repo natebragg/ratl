8582f7c3e63a728d438d7392151d6f573ea84421
natebragg <nate@cs.tufts.edu>
2017-08-08 16:17:16 -0400

    Account for head and tail, which do not have non-negativity constraints.
    In fact, head and tail have to be negative in order to reverse resource flow
    towards the list they act on.

--- src/Language/Ratl/Elab.hs
+++ src/Language/Ratl/Elab.hs
@@ -66,6 +66,12 @@ freshFunTy tau tau' = do
     q <- freshAnno
     return $ Arrow q tau tau'
 
+data Annos = Pay Anno | Exchange Anno Anno
+
+transact :: (Annos, Double) -> [(Anno, Double)]
+transact (Pay q, c) = [(q, c)]
+transact (Exchange q' q'', c) = [(q', c), (q'', negate c)]
+
 check :: Monad m => Prog -> StateT Anno (MaybeT m) (FunEnv, [Constraint])
 check fs = (,) sigma <$> concat <$> mapM (elabF . snd) fs
     where sigma = map (second tyOf) fs
@@ -78,34 +84,36 @@ check fs = (,) sigma <$> concat <$> mapM (elabF . snd) fs
                                  (ListTy pf _, ListTy p _) | p /= pf -> [([(p, 1.0), (pf, -1.0)], 0.0),
                                                                          ([(p, -1.0), (pf, 1.0)], 0.0)]
                                  _                                   -> []
-                    return $ ([(q, -1.0), (qf, 1.0)], 0.0):
-                             ([(q, 1.0), (qf, -1.0)], 0.0):equiv ++ cs
-          elabE :: Monad m => [(Var, Ty)] -> Ex -> StateT Anno (MaybeT m) (Ty, Anno, [Constraint])
+                    return $ ((qf, 1.0):transact (q, -1.0), 0.0):
+                             ((qf, -1.0):transact (q, 1.0), 0.0):equiv ++ cs
+          elabE :: Monad m => [(Var, Ty)] -> Ex -> StateT Anno (MaybeT m) (Ty, Annos, [Constraint])
           elabE gamma e = elab e
-             where elab :: Monad m => Ex -> StateT Anno (MaybeT m) (Ty, Anno, [Constraint])
+             where elab :: Monad m => Ex -> StateT Anno (MaybeT m) (Ty, Annos, [Constraint])
                    elab (Plus e1 e2)  = do (ty1, q1, cs1) <- elab e1
                                            (ty2, q2, cs2) <- elab e2
                                            q <- freshAnno
                                            guard $ isNatTy ty1 && isNatTy ty2
-                                           return (NatTy, q, ([(q, 1.0), (q1, -1.0), (q2, -1.0)], k_plus):cs1 ++ cs2)
+                                           return (NatTy, Pay q, ((q, 1.0):transact (q1, -1.0) ++ transact (q2, -1.0), k_plus):cs1 ++ cs2)
                    elab (Head e)      = do (ty, qe, cs) <- elab e
                                            guard $ isListTy ty
                                            let ListTy p ty' = ty
-                                           q <- freshAnno
-                                           return (ty', q, ([(q, 1.0), (p, 1.0), (qe, -1.0)], k_head):cs)
+                                           q' <- freshAnno
+                                           q'' <- freshAnno
+                                           return (ty', Exchange q' q'', ([(q', 1.0), (q'', -1.0), (p, 1.0)] ++ transact (qe, -1.0), k_head):cs)
                    elab (Tail e)      = do (ty, qe, cs) <- elab e
                                            guard $ isListTy ty
                                            let ListTy p _ = ty
-                                           q <- freshAnno
-                                           return (ty, q, ([(q, 1.0), (p, 1.0), (qe, -1.0)], k_tail):cs)
+                                           q' <- freshAnno
+                                           q'' <- freshAnno
+                                           return (ty, Exchange q' q'', ([(q', 1.0), (q'', -1.0), (p, 1.0)] ++ transact (qe, -1.0), k_tail):cs)
                    elab (Var x)       = do let bnd = lookup x gamma
                                            guard $ isJust bnd
                                            let Just ty = bnd
                                            q <- freshAnno
-                                           return (ty, q, [([(q, 1.0)], k_var)])
+                                           return (ty, Pay q, [([(q, 1.0)], k_var)])
                    elab (Val v)       = do ty <- elabV v
                                            q <- freshAnno
-                                           return (ty, q, [([(q, 1.0)], k_val)])
+                                           return (ty, Pay q, [([(q, 1.0)], k_val)])
                    elab (App f e)     = do (ty, qe, cs) <- elab e
                                            q <- freshAnno
                                            let sig = lookup f sigma
@@ -116,14 +124,14 @@ check fs = (,) sigma <$> concat <$> mapM (elabF . snd) fs
                                                         (ListTy p _, ListTy pf _) | p /= pf -> [([(p, 1.0), (pf, -1.0)], 0.0),
                                                                                                 ([(p, -1.0), (pf, 1.0)], 0.0)]
                                                         _                                   -> []
-                                           return (ty'', q, ([(q, 1.0), (qe, -1.0), (qf, -1.0)], k_app):equiv ++ cs)
+                                           return (ty'', Pay q, ([(q, 1.0), (qf, -1.0)] ++ transact (qe, -1.0), k_app):equiv ++ cs)
                    elab (If ep et ef) = do (typ, qp, csp) <- elab ep
                                            (tyt, qt, cst) <- elab et
                                            (tyf, qf, csf) <- elab ef
                                            q <- freshAnno
                                            guard $ eqTy tyt tyf
-                                           return (tyt, q, ([(q, 1.0), (qp, -1.0), (qt, -1.0)], k_ifp + k_ift):
-                                                           ([(q, 1.0), (qp, -1.0), (qf, -1.0)], k_ifp + k_iff):csp ++ cst ++ csf)
+                                           return (tyt, Pay q, ((q, 1.0):transact (qp, -1.0) ++ transact (qt, -1.0), k_ifp + k_ift):
+                                                               ((q, 1.0):transact (qp, -1.0) ++ transact (qf, -1.0), k_ifp + k_iff):csp ++ cst ++ csf)
           elabV :: Monad m => Val -> StateT Anno (MaybeT m) Ty
           elabV (Nat _)  = return NatTy
           elabV (List l) = elabL l
