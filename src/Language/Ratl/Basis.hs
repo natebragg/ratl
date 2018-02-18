bbaab9609a0bbc76bf0f46155bffb901bea3d149
natebragg <nate@cs.tufts.edu>
2018-02-18 07:57:20 -0500

    Add primitive greater and equal operators.

--- src/Language/Ratl/Basis.hs
+++ src/Language/Ratl/Basis.hs
@@ -31,6 +31,12 @@ plus [Nat n, Nat m] = Nat $ embed (project n + project m)
 less :: [Val] -> Val
 less [Nat n, Nat m] = Boolean $ embed $ project n < project m
 
+greater :: [Val] -> Val
+greater [Nat n, Nat m] = Boolean $ embed $ project n > project m
+
+equal :: [Val] -> Val
+equal [a, b] = Boolean $ embed $ a == b
+
 null' :: [Val] -> Val
 null' [List xs] = Boolean $ embed $ xs == Nil
 
@@ -53,6 +59,8 @@ prims = makeProg [
 
     -- comparison operations
     (V "<",     Native (Arrow ((), ()) [NatTy, NatTy] BooleanTy)                                   2 less),
+    (V ">",     Native (Arrow ((), ()) [NatTy, NatTy] BooleanTy)                                   2 greater),
+    (V "=",     Native (Arrow ((), ()) [Tyvar "a", Tyvar "a"] BooleanTy)                           2 equal),
 
     -- list functions
     (V "null?", Native (Arrow ((), ()) [ListTy [] (Tyvar "a")] BooleanTy)                          1 null'),
