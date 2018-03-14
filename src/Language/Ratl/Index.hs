619969364ad051fdf9de6b0efd3456fb147d1a8a
natebragg <nate@cs.tufts.edu>
2018-03-14 17:00:56 -0400

    Parameterize Index to be annotated with coefficients.

--- src/Language/Ratl/Index.hs
+++ src/Language/Ratl/Index.hs
@@ -7,9 +7,21 @@ module Language.Ratl.Index (
 
 import Language.Ratl.Ty (Ty(..))
 
-data Index = AIndex
-           | LIndex [Index]
+data Index q = AIndex q
+             | LIndex q [Index q]
     deriving Show
+
+instance Functor Index where
+    fmap f (AIndex q)          = AIndex (f q)
+    fmap f (LIndex q is)       = LIndex (f q) (fmap (fmap f) is)
+
+instance Foldable Index where
+    foldMap f (AIndex q)          = f q
+    foldMap f (LIndex q is)       = f q `mappend` foldMap (foldMap f) is
+
+instance Traversable Index where
+    traverse f (AIndex q)          = AIndex <$> f q
+    traverse f (LIndex q is)       = LIndex <$> f q <*> traverse (traverse f) is
 
 -- All combinations of elements of the inputs ordered by total weight, as
 -- determined by in index of the input element.  Starting with the empty
@@ -28,18 +40,18 @@ choices :: [[a]] -> [[a]]
 choices [] = [[]]
 choices (is:cs) = [i:cs' | i <- is, cs' <- choices cs]
 
-deg :: Index -> Int
-deg AIndex = 0
-deg (LIndex is) = length is + sum (map deg is)
+deg :: Index q -> Int
+deg (AIndex _) = 0
+deg (LIndex _ is) = length is + sum (map deg is)
 
-index :: Ty a -> [[Index]]
-index (NatTy)      = [[AIndex]]
-index (BooleanTy)  = [[AIndex]]
-index (UnitTy)     = [[AIndex]]
-index (SymTy)      = [[AIndex]]
+index :: Ty a -> [[Index ()]]
+index (NatTy)      = [[AIndex ()]]
+index (BooleanTy)  = [[AIndex ()]]
+index (UnitTy)     = [[AIndex ()]]
+index (SymTy)      = [[AIndex ()]]
 index (ListTy _ t) = do
     cs <- combos (index t)
-    return $ LIndex <$> (choices =<< cs)
+    return $ LIndex () <$> (choices =<< cs)
 
-indexDeg :: Int -> Ty a -> [Index]
+indexDeg :: Int -> Ty a -> [Index ()]
 indexDeg k = concat . take (k + 1) . index
