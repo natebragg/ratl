af5f99bd9fc7cfb676bed2faaee4c30e8a04e3e9
natebragg <nate@cs.tufts.edu>
2018-06-03 22:52:11 -0400

    Rewrite List and Val as type/pattern synonyms of parameterized internal types.

--- src/Language/Ratl/Val.hs
+++ src/Language/Ratl/Val.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE GADTs #-}
 
 module Language.Ratl.Val (
     Embeddable(..),
-    List(..),
+    List(Nil, Cons),
     Sym(..),
-    Val(..),
+    Val(List, Nat, Boolean, Unit, Sym),
 ) where
 
 projectionBug v = error $ "Tried to project to wrong type, which should be impossible: " ++ show v
@@ -13,6 +19,17 @@ projectionBug v = error $ "Tried to project to wrong type, which should be impos
 class Embeddable a where
     embed :: a -> Val
     project :: Val -> a
+
+newtype Fix f = Fix { unfix :: f (Fix f) }
+
+deriving instance Eq (f (Fix f)) => Eq (Fix f)
+
+instance Embeddable (f (Fix f)) => Embeddable (Fix f) where
+    embed = embed . unfix
+    project = Fix . project
+
+instance Show (f (Fix f)) => Show (Fix f) where
+    show = show . unfix
 
 newtype Nat = N Int
     deriving (Eq)
@@ -25,8 +42,8 @@ instance Embeddable Int where
 instance Show Nat where
     show (N n) = show n
 
-data List = Nil | Cons Val List
-    deriving (Eq)
+data ListRep v = NilRep | ConsRep v (ListRep v)
+    deriving (Eq, Functor)
 
 instance Embeddable a => Embeddable [a] where
     embed = List . go
@@ -38,11 +55,22 @@ instance Embeddable a => Embeddable [a] where
               go (Cons v vs) = project v:go vs
     project v = projectionBug v
 
-instance Show List where
+instance Show v => Show (ListRep v) where
     show l = beg ++ go "" l ++ end
         where (beg, sep, end) = ("(", " ", ")")
-              go _ Nil = ""
-              go c (Cons v vs) = c ++ show v ++ go sep vs
+              go _ NilRep = ""
+              go c (ConsRep v vs) = c ++ show v ++ go sep vs
+
+newtype List = ListT { unlist :: ListRep (Fix ValRep) }
+    deriving Eq
+
+{-# COMPLETE Nil, Cons #-}
+
+pattern Nil = ListT NilRep
+pattern Cons v xs <- ((\case
+            l@(ListT (ConsRep v xs)) -> (l, Val v, ListT xs)
+            l -> (l, undefined, undefined)) -> (ListT (ConsRep _ _), v, xs))
+    where Cons v xs = ListT (ConsRep (unval v) (unlist xs))
 
 newtype Boolean = B Bool
     deriving (Eq)
@@ -72,20 +100,41 @@ instance Embeddable Sym where
     project (Sym x) = x
     project v = projectionBug v
 
-data Val = List List
-         | Nat Nat
-         | Boolean Boolean
-         | Unit
-         | Sym Sym
-    deriving (Eq)
+data ValRep v = ListRep (ListRep v)
+              | NatRep Nat
+              | BooleanRep Boolean
+              | UnitRep
+              | SymRep Sym
+    deriving (Eq, Functor)
+
+instance Embeddable v => Embeddable (ValRep v) where
+    embed = Val . Fix . fmap (unval . embed)
+    project = fmap (project . Val) . unfix . unval
+
+instance Show v => Show (ValRep v) where
+    show (ListRep xs) = show xs
+    show (NatRep n) = show n
+    show (BooleanRep b) = show b
+    show UnitRep = show ()
+    show (SymRep s) = show s
+
+newtype Val = Val { unval :: (Fix ValRep) }
+    deriving Eq
+
+{-# COMPLETE List, Nat, Boolean, Unit, Sym #-}
+
+pattern List xs <- ((\case
+            v@(Val (Fix (ListRep xs))) -> (v, ListT xs)
+            v -> (v, undefined)) -> (Val (Fix (ListRep _)), xs))
+    where List xs = Val (Fix (ListRep (unlist xs)))
+pattern Nat n = Val (Fix (NatRep n))
+pattern Boolean b = Val (Fix (BooleanRep b))
+pattern Unit = Val (Fix UnitRep)
+pattern Sym s = Val (Fix (SymRep s))
 
 instance Embeddable Val where
     embed = id
     project = id
 
 instance Show Val where
-    show (List xs) = show xs
-    show (Nat n) = show n
-    show (Boolean b) = show b
-    show (Sym s) = show s
-    show Unit = show ()
+    show = show . unval
