ea74a33202782c353eaf41ad4da24c94fd014676
natebragg <nate@cs.tufts.edu>
2019-02-24 20:15:51 -0500

    Housekeeping for quantifiers.

--- src/Language/Ratl/Elab.hs
+++ src/Language/Ratl/Elab.hs
@@ -71,15 +71,13 @@ class Quantified t where
 alpha :: (Quantified t, Monad m) => Tyvar -> t -> FreshTyvar m t
 alpha x t = (\t' -> subst [(x, t')] t) <$> freshTyvar
 
-refresh :: (Quantified t, Monad m) => t -> FreshTyvar m t
-refresh t = foldrM alpha t $ freeVars t
-
 compose :: TyvarEnv -> TyvarEnv -> TyvarEnv
-compose theta2 theta1 = unionBy ((==) `on` fst) (fmap (fmap (subst theta2)) theta1) theta2
+compose theta2 theta1 = unionBy ((==) `on` fst) (subst theta2 theta1) theta2
 
 class Quantified t => Unifiable t where
     uid :: t
     (~~) :: MonadError TypeError m => t -> t -> FreshTyvar m TyvarEnv
+    freshTy :: Monad m => t -> FreshTyvar m t
 
 infix 4 ~~
 
@@ -93,7 +91,7 @@ unify t t' = either (error . show) (flip subst t') $ solve t t'
 
 instance Quantified Ty where
     freeVars    (ListTy ty) = freeVars ty
-    freeVars (PairTy t1 t2) = freeVars t1 ++ freeVars t2
+    freeVars (PairTy t1 t2) = nub $ freeVars t1 ++ freeVars t2
     freeVars      (Tyvar y) = [y]
     freeVars              _ = []
 
@@ -117,6 +115,10 @@ instance Unifiable Ty where
         return $ theta2 `compose` theta1
     t' ~~ t = throwError $ TypeError $ [(t', t)]
 
+    freshTy (ListTy ty   ) = ListTy <$> freshTy ty
+    freshTy (PairTy t1 t2) = PairTy <$> freshTy t1 <*> freshTy t2
+    freshTy ty             = return ty
+
 instance (Functor f, Foldable f, Quantified a) => Quantified (f a) where
     freeVars = concatMap freeVars
 
@@ -133,6 +135,9 @@ instance Unifiable [Ty] where
         theta2 <- subst theta1 ts ~~ subst theta1 t's
         return $ theta2 `compose` theta1
 
+    -- [Ty] is implicitly a universally quantified closure
+    freshTy ty = traverse freshTy =<< foldrM alpha ty (freeVars ty)
+
 instance Quantified FunTy where
     freeVars (Arrow t t') = freeVars t ++ freeVars t'
 
@@ -145,6 +150,11 @@ instance Unifiable FunTy where
         theta1 <- t1 ~~ t3
         theta2 <- subst theta1 t2 ~~ subst theta1 t4
         return $ theta2 `compose` theta1
+
+    -- FunTy is implicitly a universally quantified closure
+    freshTy ty = do
+        Arrow t1 t2 <- foldrM alpha ty $ freeVars ty
+        Arrow <$> traverse freshTy t1 <*> freshTy t2
 
 instance Quantified Fun where
     freeVars = freeVars . tyOf
@@ -188,7 +198,7 @@ instance Elab Prog where
 instance Elab Fun where
     type Type Fun = TypedFun
     elab (Fun fty xs e) = do
-        Arrow pty rty <- refresh fty
+        Arrow pty rty <- freshTy fty
         when (length xs /= length pty) $
             throwError $ ArityError (length xs) (length pty)
         ety <- local (\ce -> ce {gamma = zip xs pty}) $ elab e
@@ -201,7 +211,7 @@ instance Elab Fun where
 instance Elab Ex where
     type Type Ex = TypedEx
     elab (Var x) = do
-        ty <- unlessJustM (asks $ lookup x . gamma) $
+        ty <- freshTy <=< unlessJustM (asks $ lookup x . gamma) $
             throwError $ NameError x
         return $ TypedVar ty x
     elab (Val v) = do
@@ -209,13 +219,13 @@ instance Elab Ex where
         return $ TypedVal ty v
     elab (If ep et ef) = do
         etys <- traverse elab [ep, et, ef]
-        ifty <- refresh [BooleanTy, uid, uid]
+        ifty <- freshTy [BooleanTy, uid, uid]
         theta <- ifty ~~ map tyGet etys
         let [etyp', etyt', etyf'] = subst theta etys
         return $ TypedIf (tyGet etyf') etyp' etyt' etyf'
     elab (App f es) = do
         etys <- traverse elab es
-        Arrow ty ty' <- refresh <=< unlessJustM (asks $ lookup f . phi) $
+        Arrow ty ty' <- freshTy <=< unlessJustM (asks $ lookup f . phi) $
             throwError $ NameError f
         when (length ty /= length es) $
             throwError $ ArityError (length ty) (length es)
